0d4463ec9e8e94d2c0e09e071c586848a4bbb0d6
parveensohi <39779009+parveensohi@users.noreply.github.com>
2018-05-30 16:37:00 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author amandeep
+ */
+public class HelloWorld {
+   public static void main(String args[]){
+       System.out.println("Hello Java");
+       int n1 = 10;
+       float f1 = 10.5f;
+       double d1 = 12.345456677;
+       short s1 =12;
+       char c1 = 'a';
+       String str1 = "java";
+       boolean b1 = true;
+       System.out.println("value of n1 is " + n1);
+       System.out.println("value of f1 is" +f1);
+       System.out.println("value of d1 is " +d1);
+       System.out.println("value of s1 is " + s1);
+       System.out.println("value of c1 is"+ c1);
+       System.out.println("value of str1 is "+ str1);
+   } 
+}//<editor-fold defaultstate="collapsed" desc="comment">
+
+//</editor-fold>
